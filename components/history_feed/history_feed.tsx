5f407fafc363d580d2cf832541f306f07260f84a
varmamsp <varmamsp111@gmail.com>
2020-03-21 23:52:28 +0530

    Fix history feed

--- components/history_feed/history_feed.tsx
+++ components/history_feed/history_feed.tsx
@@ -47,7 +47,7 @@ const HistoryFeed: React.FC<StateToProps & DispatchToProps> = ({
       <hr className="mb-3" />
       {history.map((episode) => (
         <div key={episode.id} className="mb-6">
-          <EpisodePreview episodeId={episode.id} />
+          <EpisodePreview episodeId={episode.id} small />
         </div>
       ))}
       {!receivedAll && (
