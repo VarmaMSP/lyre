fb3460cba3c177fb8ce182d7a006721942074c5e
varmamsp <varmamsp111@gmail.com>
2020-03-25 22:27:10 +0530

    Add infinite scroll in subscriptions feed

--- components/subscriptions_feed/subscriptions_feed.tsx
+++ components/subscriptions_feed/subscriptions_feed.tsx
@@ -1,6 +1,7 @@
-import ButtonShowMore from 'components/button_show_more'
 import EpisodePreview from 'components/episode_preview'
+import useVisible from 'hooks/useVisible'
 import { Episode } from 'models'
+import React, { useEffect } from 'react'
 
 export interface StateToProps {
   feed: Episode[]
@@ -18,6 +19,12 @@ const SubscriptionsFeed: React.SFC<StateToProps & DispatchToProps> = ({
   isLoadingMore,
   loadMore,
 }) => {
+  const [reference, isVisible] = useVisible()
+
+  useEffect(() => {
+    isVisible && loadMore(feed.length)
+  }, [isVisible])
+
   if (feed.length === 0) {
     return (
       <div className="mt-8">
@@ -37,18 +44,27 @@ const SubscriptionsFeed: React.SFC<StateToProps & DispatchToProps> = ({
         {'Feed'}
       </h1>
       <hr className="mb-3" />
+
+      {/* Feed */}
       {feed.map((episode) => (
         <div key={episode.id} className="mb-6">
           <EpisodePreview episodeId={episode.id} small />
         </div>
       ))}
-      {!receivedAll && (
-        <div className="w-full h-10 mx-auto my-6">
-          <ButtonShowMore
-            isLoading={isLoadingMore}
-            loadMore={() => loadMore(feed.length)}
-          />
-        </div>
+
+      {/* Auto Load */}
+      {!receivedAll && !isLoadingMore && (
+        <div className="w-full h-10" ref={reference} />
+      )}
+
+      {/* Loader */}
+      {!receivedAll && isLoadingMore && (
+        <div className="spinner mx-auto my-8" />
+      )}
+
+      {/* finished Loading */}
+      {receivedAll && (
+        <div className="w-full h-10 my-6 text-center">{'Thats all, Folks'}</div>
       )}
     </div>
   )
