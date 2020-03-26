3b9d43f878358af1f853fa9f44b63e6f99bbf4fc
varmamsp <varmamsp111@gmail.com>
2020-03-26 12:08:09 +0530

    Minor UI change

--- components/explore_view/recommended/recommended.tsx
+++ components/explore_view/recommended/recommended.tsx
@@ -16,17 +16,21 @@ const Recommended: React.FC<StateToProps> = ({ podcasts }) => {
         {'Recommended'}
       </h1>
       <hr className="mb-3" />
-      <Grid cols={{ SM: 4, MD: 6, LG: 10 }}>
+
+      <Grid cols={{ SM: 4, MD: 6, LG: 8 }}>
         {podcasts.map((p) => (
-          <div key={p.id} className="flex-none px-1 mb-4">
+          <div
+            key={p.id}
+            className="flex-none px-2 py-2 mb-4 hover:bg-gray-100 rounded-lg"
+          >
             <PodcastLink podcastUrlParam={p.urlParam}>
-              <a className="block flex-none mb-2">
-                <Img src={getImageUrl(p.urlParam)} />
-              </a>
-            </PodcastLink>
-            <PodcastLink podcastUrlParam={p.urlParam}>
-              <a className="text-2xs text-gray-900 tracking-wide font-medium leading-snug line-clamp-2">
-                {p.title}
+              <a className="block w-full h-full text-gray-900 hover:text-blue-700">
+                <div className="mb-2">
+                  <Img src={getImageUrl(p.urlParam)} />
+                </div>
+                <div className="text-2xs tracking-wide font-medium leading-snug line-clamp-2">
+                  {p.title}
+                </div>
               </a>
             </PodcastLink>
           </div>
