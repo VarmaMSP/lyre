1f4bc8c8f621503e024738e8599adc28fc1d37bc
varmamsp <varmamsp111@gmail.com>
2020-04-03 09:37:38 +0530

    Modify Disable Scroll hook

--- hooks/useDisableScroll.ts
+++ hooks/useDisableScroll.ts
@@ -1,21 +1,55 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 
 function useDisableScroll(disableScroll: boolean) {
-  useEffect(() => {
-    const fn: EventListener = (() => {
-      const winX = window.scrollX
-      const winY = window.scrollY
+  const passiveSupported = useMemo(() => {
+    let res: boolean = false
+    let fn: EventListener = () => {}
+    let opts: AddEventListenerOptions = {
+      get passive() {
+        res = true
+        return false
+      },
+    }
 
-      return () => window.scrollTo(winX, winY)
-    })()
+    try {
+      window.addEventListener('test', fn, opts)
+      window.removeEventListener('test', fn, opts)
+    } catch (err) {}
+
+    return res
+  }, [])
+
+  useEffect(() => {
+    const fn1: EventListener = (e) => {
+      e.preventDefault()
+    }
+
+    const fn2: EventListener = (e) => {
+      const k: number = (e as any).keyCode
+      if (k === 32 || k === 37 || k === 38 || k === 39 || k === 40) {
+        e.preventDefault()
+        return false
+      }
+    }
+
+    const opts: AddEventListenerOptions | boolean = passiveSupported
+      ? { passive: false }
+      : false
 
     const cleanUp = () => {
-      window.removeEventListener('scroll', fn)
+      window.removeEventListener('DOMMouseScroll', fn1, false)
+      window.removeEventListener('wheel', fn1, opts)
+      window.removeEventListener('touchmove', fn1, opts)
+      window.removeEventListener('keydown', fn2, false)
     }
 
     cleanUp()
     if (disableScroll) {
-      window.addEventListener('scroll', fn)
+      window.addEventListener('DOMMouseScroll', fn1, false)
+      window.addEventListener('wheel', fn1, opts)
+      window.addEventListener('touchmove', fn1, opts)
+      window.addEventListener('keydown', fn2, false)
+
       return cleanUp
     }
   }, [disableScroll])
