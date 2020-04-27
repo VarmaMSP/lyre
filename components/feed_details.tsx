e919c4fe4ad0901f12928a5b6d288740d1023294
varmamsp <varmamsp111@gmail.com>
2020-04-27 12:00:34 +0530

    MInor update

--- components/feed_details.tsx
+++ components/feed_details.tsx
@@ -18,7 +18,7 @@ const FeedDetails: React.FC<OwnProps> = ({ podcast }) => {
           RSS
         </a>
       </div>
-      <div className="mt-2 text-gray-800 text-xs">{`Feed refreshed ${formatDistanceToNow(
+      <div className="mt-2 text-gray-800 text-xs">{`Last updated ${formatDistanceToNow(
         parseISO(podcast.feedLastRefreshAt),
       )} ago`}</div>
 
